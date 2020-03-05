fdb052b28eca9ed4f7d55d9fb06acaadd651fde5
Marcelo Wendel <mwbc.cs@gmail.com>
2020-03-05 17:10:22 -0300

    processando campos antes da validacao

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 
